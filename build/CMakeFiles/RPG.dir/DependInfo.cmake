755ab7c05acda7805dea90ebec001724aa7b63ed
nicola balao <nicolasbalao2@gmail.com>
2021-10-13 17:39:53 +0200

    CMakeList.txt

---
+++ build/CMakeFiles/RPG.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tengu/Documents/Formation/C++/tp/PooRPG/source/Arme.cpp" "/home/tengu/Documents/Formation/C++/tp/PooRPG/build/CMakeFiles/RPG.dir/source/Arme.cpp.o"
+  "/home/tengu/Documents/Formation/C++/tp/PooRPG/source/Personnage.cpp" "/home/tengu/Documents/Formation/C++/tp/PooRPG/build/CMakeFiles/RPG.dir/source/Personnage.cpp.o"
+  "/home/tengu/Documents/Formation/C++/tp/PooRPG/source/Samourai.cpp" "/home/tengu/Documents/Formation/C++/tp/PooRPG/build/CMakeFiles/RPG.dir/source/Samourai.cpp.o"
+  "/home/tengu/Documents/Formation/C++/tp/PooRPG/source/Sorcier.cpp" "/home/tengu/Documents/Formation/C++/tp/PooRPG/build/CMakeFiles/RPG.dir/source/Sorcier.cpp.o"
+  "/home/tengu/Documents/Formation/C++/tp/PooRPG/source/Tank.cpp" "/home/tengu/Documents/Formation/C++/tp/PooRPG/build/CMakeFiles/RPG.dir/source/Tank.cpp.o"
+  "/home/tengu/Documents/Formation/C++/tp/PooRPG/source/main.cpp" "/home/tengu/Documents/Formation/C++/tp/PooRPG/build/CMakeFiles/RPG.dir/source/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/RPG.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPG.dir/source/Arme.cpp.o"
+  "CMakeFiles/RPG.dir/source/Personnage.cpp.o"
+  "CMakeFiles/RPG.dir/source/Samourai.cpp.o"
+  "CMakeFiles/RPG.dir/source/Sorcier.cpp.o"
+  "CMakeFiles/RPG.dir/source/Tank.cpp.o"
+  "CMakeFiles/RPG.dir/source/main.cpp.o"
+  "RPG"
+  "RPG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
